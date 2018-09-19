fixed normalization issue globally

src/CI_base.py
@@ -81,10 +81,15 @@ class CI_base(object):
         assert (self.nz == self.ny), "Size Mismatch"
         assert (self.nx == self.nz), "Size Mismatch"
 
+        if self.normalized:
+            X = scale(X,axis=0,with_mean = False)
+            Y = scale(Y,axis=0,with_mean = False)
+            Z = scale(Z,axis=0,with_mean = False)
+
     def CI_classify(self):
         np.random.seed(11)
         if self.train_samp == -1:
-            self.num_train = int(4.0*self.nx/3.0)
+            self.num_train = int(2.0*self.nx/3.0)
             Xtrain,Ytrain,Xtest,Ytest = self.mimic_sampler(self.X,self.Y,self.Z, self.param_dict)
         else:
             self.num_train = 2*self.train_samp*self.nx
@@ -94,38 +99,42 @@ class CI_base(object):
         if self.dc:
             params = self.params
             model = MLP(params,Xtrain.shape[1],2,cudaEfficient=True)
-        else:
-            model,features,bp = XGB_crossvalidated_model(max_depths=self.max_depths, n_estimators=self.n_estimators, \
-                colsample_bytrees=self.colsample_bytrees,Xtrain=Xtrain,Ytrain=Ytrain,nfold=self.nfold,feature_selection = 0,nthread = self.nthread)
-        if self.dc:
             model.fit(Xtrain,Ytrain.astype(int),validation_split=0.2)
             gbm = model
-        else:
-            gbm = model.fit(Xtrain,Ytrain)
-        try:
-            pred = gbm.predict_proba(Xtest)
-        except:
             pred = gbm.predict_proba(torch.from_numpy(Xtest).float())
-        pred_exact = np.argmax(pred,axis =1)
-        acc1 = accuracy_score(Ytest, pred_exact)
-        AUC1 = roc_auc_score(Ytest,pred[:,1])
-        del gbm
-        if self.dc:
-            params = self.params
+            pred_exact = np.argmax(pred,axis =1)
+            acc1 = accuracy_score(Ytest, pred_exact)
+            AUC1 = roc_auc_score(Ytest,pred[:,1])
+            del gbm
             model = MLP(params,Xtrain[:,self.dx::].shape[1],2,cudaEfficient=True)
-        if self.dc:
             model.fit(Xtrain[:,self.dx::],Ytrain.astype(int),validation_split=0.2)
             gbm = model
-        else:
-            gbm = model.fit(Xtrain[:,self.dx::],Ytrain)
-        try:
-            pred = gbm.predict_proba(Xtest[:,self.dx::])
-        except:
             pred = gbm.predict_proba(torch.from_numpy(Xtest[:,self.dx::]).float())
-        pred_exact = np.argmax(pred,axis =1)
-        acc2 = accuracy_score(Ytest, pred_exact)
-        AUC2 = roc_auc_score(Ytest,pred[:,1])
-        del gbm
+            pred_exact = np.argmax(pred,axis =1)
+            acc2 = accuracy_score(Ytest, pred_exact)
+            AUC2 = roc_auc_score(Ytest,pred[:,1])
+            print 'Using Deep Model: ',
+            print gbm
+            del gbm
+        else:
+            model,features,bp = XGB_crossvalidated_model(max_depths=self.max_depths, n_estimators=self.n_estimators, \
+                colsample_bytrees=self.colsample_bytrees,Xtrain=Xtrain,Ytrain=Ytrain,\
+                                                         nfold=self.nfold,feature_selection = 0,nthread = self.nthread)
+            gbm = model.fit(Xtrain,Ytrain)
+            pred = gbm.predict_proba(Xtest)
+            pred_exact = np.argmax(pred,axis =1)
+            acc1 = accuracy_score(Ytest, pred_exact)
+            AUC1 = roc_auc_score(Ytest,pred[:,1])
+            del gbm
+            gbm = model.fit(Xtrain[:,self.dx::],Ytrain)
+            pred = gbm.predict_proba(Xtest[:,self.dx::])
+            pred_exact = np.argmax(pred,axis =1)
+            acc2 = accuracy_score(Ytest, pred_exact)
+            AUC2 = roc_auc_score(Ytest,pred[:,1])
+            print 'Using XGB model: '
+            print gbm
+            del gbm
+        
         x = [0.0, AUC1 - AUC2 , AUC2 - 0.5, acc1 - acc2, acc2 - 0.5]
         print 'AC_w_x: ' + str(AUC1),
         print 'AC_no_x: ' + str(AUC2)

src/MIMIFY_GAN.py
@@ -87,7 +87,7 @@ def train_conditional_gan_original(data,dim_N,dim_X,dim_Y,dim_Z, max_epoch, BSIZ
     print 'MAX ITER: ' + str(max_iter)
     print 'in train GAN',
     print normalized
-    Data = data_iterator(dx=dim_X,dy=dim_Y,dz=dim_Z,sType = 'CI',size = 10000,bsize = BSIZE,nstd = 0.5,fixed_z = False,data = data,normalized=normalized)
+    Data = data_iterator(dx=dim_X,dy=dim_Y,dz=dim_Z,bsize = BSIZE,nstd = 0.5,data = data,normalized=normalized)
     netG = Generator(dim_N,dim_Y,dim_Z,dim_Z+dim_N)
     netD = Discriminator_original(dim_X,dim_Y,dim_Z,dim_Z+dim_Y)
     criterion = nn.BCELoss()
@@ -196,12 +196,14 @@ def CI_sampler_conditional_CGAN(X_in,Y_in,Z_in,param_dict):
     normalized = param_dict['normalized']
     print 'in CI Sampler', 
     print normalized
-    samples = np.hstack([X_in,Y_in,Z_in]).astype(np.float32)
+    
 
     if normalized:
         X_in = scale(X_in,axis = 0,with_mean = False)
         Y_in = scale(Y_in,axis = 0,with_mean = False)
         Z_in = scale(Z_in,axis = 0,with_mean = False)
+
+    samples = np.hstack([X_in,Y_in,Z_in]).astype(np.float32)
 
     if train_len == -1:
         #train_len = 4*len(X_in)/3

src/MIMIFY_REG.py
@@ -57,7 +57,7 @@ def train_regressor(data,dim_X,dim_Y,dim_Z, max_epoch, BSIZE,option = 1, normali
     max_iter = max_epoch*n/BSIZE + 1
     print 'MAX ITER: ' + str(max_iter)
     print 'in train REG'
-    Data = data_iterator(dx=dim_X,dy=dim_Y,dz=dim_Z,sType = 'CI',size = 10000,bsize = BSIZE,nstd = 0.5,fixed_z = False,data = data,normalized=normalized)
+    Data = data_iterator(dx=dim_X,dy=dim_Y,dz=dim_Z,bsize = BSIZE,nstd = 0.5,data = data,normalized=normalized)
     netG = Regressor(dim_Y,dim_Z,DIM)
     netG.apply(weights_init)
     criterion = nn.L1Loss()
@@ -285,7 +285,8 @@ def CI_sampler_regressor_v2(X_in,Y_in,Z_in,param_dict):
     data1= samples[0:nx/2,:]
     data2 = samples[nx/2::,:]
 
-    multioutputregressor = MultiOutputRegressor(estimator=xgb.XGBRegressor(objective='reg:linear',max_depth=max_depth, colsample_bytree= 1.0, n_estimators=n_estimators,nthread=nthread))
+    multioutputregressor = MultiOutputRegressor(estimator=xgb.XGBRegressor(objective='reg:linear',max_depth=max_depth, \
+        colsample_bytree= 1.0, n_estimators=n_estimators,nthread=nthread))
 
     Xset = range(0,dx)
     Yset = range(dx,dx + dy)
@@ -303,7 +304,7 @@ def CI_sampler_regressor_v2(X_in,Y_in,Z_in,param_dict):
 
     nz1,mz1 = Z1.shape
 
-    mults = [1e-4,1e-3,1e-2,1e-1,1.0]
+    mults = [1e-5,1e-4,1e-3,1e-2,1e-1]
     sigmas = [l/np.sqrt(dz) for l in mults]
 
     mini = 1e6
@@ -409,7 +410,7 @@ def CI_sampler_regressor_v3(X_in,Y_in,Z_in,param_dict):
     print 'Calculated Covariance: ',
     print cov 
 
-    mults = [1e-4,1e-3,1e-2,1e-1,1.0]
+    mults = [1e-4,1e-3,1e-2,1e-1]
     sigmas = [l/np.sqrt(mz1) for l in mults]
     
 

src/utilities.py
@@ -40,39 +40,39 @@ class data_iterator(object):
     def __init__(self,dx,dy,dz,sType = 'CI',size = 10000,bsize = 50,nstd = 0.5,fixed_z = False,data = None,channel = 1,normalized = False ):
         if data is not None:
             self.dataset = data
-        else:
-            cov = np.eye(dz)
-            mu = np.ones(dz)
-            if fixed_z:
-                Z = list(np.random.multivariate_normal(mu,cov,1))*size
-                Z = np.array(Z)
-            else:
-                Z = np.random.multivariate_normal(mu,cov,size)
-            Z = np.matrix(Z)
-            Ax = np.random.rand(dz,dx)
-            for i in range(dx):
-                Ax[:,i] = Ax[:,i]/np.linalg.norm(Ax[:,i])
-            Ax = np.matrix(Ax)
-            Ay = np.random.rand(dz,dy)
-            for i in range(dy):
-                Ay[:,i] = Ay[:,i]/np.linalg.norm(Ay[:,i])
-            Ay = np.matrix(Ay)
+        # else:
+        #     cov = np.eye(dz)
+        #     mu = np.ones(dz)
+        #     if fixed_z:
+        #         Z = list(np.random.multivariate_normal(mu,cov,1))*size
+        #         Z = np.array(Z)
+        #     else:
+        #         Z = np.random.multivariate_normal(mu,cov,size)
+        #     Z = np.matrix(Z)
+        #     Ax = np.random.rand(dz,dx)
+        #     for i in range(dx):
+        #         Ax[:,i] = Ax[:,i]/np.linalg.norm(Ax[:,i])
+        #     Ax = np.matrix(Ax)
+        #     Ay = np.random.rand(dz,dy)
+        #     for i in range(dy):
+        #         Ay[:,i] = Ay[:,i]/np.linalg.norm(Ay[:,i])
+        #     Ay = np.matrix(Ay)
 
-            Axy = np.random.rand(dx,dy)
-            for i in range(dy):
-                Axy[:,i] = Axy[:,i]/np.linalg.norm(Axy[:,i])
-            Axy = np.matrix(Axy)
+        #     Axy = np.random.rand(dx,dy)
+        #     for i in range(dy):
+        #         Axy[:,i] = Axy[:,i]/np.linalg.norm(Axy[:,i])
+        #     Axy = np.matrix(Axy)
 
-            if sType == 'CI':
-                X = Z*Ax + nstd*np.random.multivariate_normal(np.zeros(dx),np.eye(dx),size)
-                Y = Z*Ay + nstd*np.random.multivariate_normal(np.zeros(dy),np.eye(dy),size)
-            elif sType == 'I':
-                X = nstd*np.random.multivariate_normal(np.zeros(dx),np.eye(dx),size)
-                Y = nstd*np.random.multivariate_normal(np.zeros(dy),np.eye(dy),size)
-            else:
-                X = np.random.multivariate_normal(np.zeros(dx),np.eye(dx),size)
-                Y = 2*X*Axy + Z*Ay + nstd*np.random.multivariate_normal(np.zeros(dy),np.eye(dy),size)
-            self.dataset = np.array(np.hstack([X,Y,Z])).astype(np.float32)
+        #     if sType == 'CI':
+        #         X = Z*Ax + nstd*np.random.multivariate_normal(np.zeros(dx),np.eye(dx),size)
+        #         Y = Z*Ay + nstd*np.random.multivariate_normal(np.zeros(dy),np.eye(dy),size)
+        #     elif sType == 'I':
+        #         X = nstd*np.random.multivariate_normal(np.zeros(dx),np.eye(dx),size)
+        #         Y = nstd*np.random.multivariate_normal(np.zeros(dy),np.eye(dy),size)
+        #     else:
+        #         X = np.random.multivariate_normal(np.zeros(dx),np.eye(dx),size)
+        #         Y = 2*X*Axy + Z*Ay + nstd*np.random.multivariate_normal(np.zeros(dy),np.eye(dy),size)
+        #     self.dataset = np.array(np.hstack([X,Y,Z])).astype(np.float32)
         #print normalized
         if normalized is True:
             self.dataset = scale(self.dataset,axis = 0,with_mean = False)