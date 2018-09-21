859f83cfc5a99b0939676ade91f9de7aa959abec
Rajat Sen <rsen@c227-104.maverick2.tacc.utexas.edu>
2018-09-21 15:06:36 -0500

    bash files added

--- example.sh
+++ example.sh
@@ -1,1 +1,1 @@
-python run_mimify_GAN.py -fp './data/dim70_random/' -dx 1 -dy 1 -dz 70 -nf 100 -me 50 -bs 50 -rf 'test'  -dc 1 -nm 1 
+python run_mimify_GAN.py -fp '/work/03782/rsen/maverick2/mimicdata/dim20_random/' -dx 1 -dy 1 -dz 20 -nf 200 -me 50 -bs 50 -rf 'results_mimify_gan_xgb'  -dc 0 -nm 1 -dm 20
---
+++ example_reg.sh
@@ -0,0 +1,1 @@
+python run_mimify_reg.py -fp '/work/03782/rsen/maverick2/mimicdata/dim70_random/' -dx 1 -dy 1 -dz 70 -nf 200 -me 200 -bs 200 -rf 'results_mimify_reg_deep_xgb'  -dc 0 -nm 1 -dp 1
